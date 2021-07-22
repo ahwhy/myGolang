e14c0e9175c3c03b588d6e9c5411500cdd14220e
atlantis <ahwhya@outlook.con>
2021-07-22 20:50:30 +0800

    补交和上传作业

---
+++ week7/practice/log/ilog/ilog.go
@@ -0,0 +1,31 @@
+package ilog
+
+import (
+	"os"
+
+	"github.com/sirupsen/logrus"
+)
+
+var Logger = logrus.New()
+
+func InitLogger(level string) {
+	// 设置日志等级
+	switch level {
+	case "info":
+		Logger.SetLevel(logrus.InfoLevel)
+	case "debug":
+		Logger.SetLevel(logrus.DebugLevel)
+	case "warn":
+		Logger.SetLevel(logrus.WarnLevel)
+	case "error":
+		Logger.SetLevel(logrus.ErrorLevel)
+	}
+	// 设置日志输出路径
+	Logger.SetOutput(os.Stdout)
+	// 设置filename
+	Logger.SetReportCaller(true)
+	// 设置格式化文本
+	Logger.SetFormatter(&logrus.TextFormatter{
+		TimestampFormat: "2006/01/02 15:04:05",
+	})
+}
